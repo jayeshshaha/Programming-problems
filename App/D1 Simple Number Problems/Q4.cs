2117c27784aeb6324326fba8665cd9531ee2a1c4
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-20 22:13:34 +0530

    Q4

---
+++ App/D1 Simple Number Problems/Q4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D1_Simple_Number_Problems
+{
+    // Sum of First N Natural Numbers
+    internal class Q4 
+    {
+        public static void Execute()
+        {
+            int n = 5;    
+            int sum = 0;  
+            while (n != 0) 
+            {
+                sum = sum + n;
+                n--;
+
+            }
+            Console.WriteLine(sum);
+        }
+        
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -6,7 +6,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Q3.Execute();
+            Q4.Execute();
         }
     }
 }
