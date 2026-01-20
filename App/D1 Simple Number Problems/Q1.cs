785410ed573ebbdaf4d5eaefa5f72aa8ea17e9c9
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-20 21:22:51 +0530

    Add project files.

---
+++ App/D1 Simple Number Problems/Q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D1_Simple_Number_Problems
+{
+    // Print Numbers from 1 to N
+    public static class Q1
+    {
+        public static void Execute()
+        {
+            int n = 5;
+            int start = 1;
+            while (start <= n)
+            {
+                Console.WriteLine(start);
+                start++;              
+            }
+        }
+
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using App.D1_Simple_Number_Problems;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Q1.Execute();
+        }
+    }
+}
