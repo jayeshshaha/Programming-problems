9625449271f2f016565b386eff3b304e69340a34
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-20 21:30:22 +0530

    Q2

---
+++ App/D1 Simple Number Problems/Q2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D1_Simple_Number_Problems
+{
+    // Print Numbers from N to 1 without changing the loop condition of above question
+    internal static class Q2
+    {
+        public static void Execute()
+        {
+            int n = 5;
+            int start = 1;
+            while (start <= n)
+            {
+                Console.WriteLine(n);
+                n--;
+            }
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -6,7 +6,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Q1.Execute();
+            Q2.Execute();
         }
     }
 }
