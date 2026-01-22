e4bac4c9ca7555f6c9fd2091ad70068c0916b6af
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-22 22:33:58 +0530

    Q7

---
+++ App/D1 Simple Number Problems/Q7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D1_Simple_Number_Problems
+{
+    // Print Squares of Numbers from 1 to N
+    internal class Q7
+    {
+        public static void Execute()
+        {
+            int n = 5;
+            int start = 1;
+            while (start <= n)
+            {
+                int square = start * start;
+                Console.Write(square + " ");
+                start++;
+            }
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -6,7 +6,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Q6.Execute();
+            Q7.Execute();
         }
     }
 }
