9addf7e475d72339a7ecad8740e4b0f7ee3215f5
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-23 17:41:15 +0530

    Q10

---
+++ App/D1 Simple Number Problems/Q10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D1_Simple_Number_Problems
+{
+    // Print the cubes of numbers from 1 to N
+    internal class Q10
+    {
+        public static void Execute()
+        {
+            int n = 5;
+            int start = 1;
+            while (start <= n)
+            {
+                int cube = start * start * start;
+                Console.WriteLine(cube + " ");
+                start++;
+            }
+        }
+    }
+}
