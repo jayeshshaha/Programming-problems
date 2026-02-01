e9d27c78e9fd14bb88bbaa626eb4ff24f9c1f03c
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-02-01 20:01:19 +0530

    Q21

---
+++ App/D3  Loops & Patterns/Q21.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D3__Loops___Patterns
+{
+    // Print Inverted Right-Angled Triangle
+    internal class Q21
+    {
+        public static void Execute()
+        {
+            int row = 5;
+            for (int i = row; i >= 1; i--)
+            {
+                for (int col = 1; col <= i; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- App/Questions.md
+++ App/Questions.md
@@ -26,3 +26,4 @@
 
 # D3 - Loops & Patterns
 20. Print Right-Angled Star Triangle
+21. Print Inverted Right-Angled Triangle
