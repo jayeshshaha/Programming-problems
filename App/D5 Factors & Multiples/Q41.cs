2bb9838ba3d4bc4bab2a8b4f5345c5f1db7995dd
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-02-22 14:31:48 +0530

    Q41

---
+++ App/D5 Factors & Multiples/Q41.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D5_Factors___Multiples
+{
+    // Print All Factors of a Number
+    internal class Q41
+    {
+        public static void Execute()
+        {
+            int n = 24;
+            List<int> list = new List<int>();   
+            for (int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    list.Add(i);
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
