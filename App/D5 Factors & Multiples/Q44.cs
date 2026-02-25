6f8329b8cf55277e71c6b854f6f7dd63aa6d774b
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-02-25 23:56:30 +0530

    Q44

---
+++ App/D5 Factors & Multiples/Q44.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.D5_Factors___Multiples
+{
+    // Find the LCM (Least Common Multiple) of Two Numbers
+    internal class Q44
+    {
+        public static void Execute()
+        {
+            int num1 = 12; 
+            int num2 = 18; 
+
+      
+            List<int> list = new List<int>();
+            for (int i = 1; i <= num1; i++)
+            {
+                if (num1 % i == 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            int hcf = 1;
+            foreach (var item in list)
+            {
+                if (num2 % item == 0)
+                {
+                    hcf = item;
+
+                }
+            }
+
+            int lcm = (num1 * num2) / hcf;
+            Console.WriteLine(lcm);
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -10,7 +10,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-           Q43.Execute();
+           Q44.Execute();
         }
     }
 }
